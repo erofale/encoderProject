351197508f674fc329da35ccae2ce4b5e8a00874
Artyom <ar.buyanow@yandex.ru>
2021-12-02 23:01:09 +0300

    fix

--- Code/Scripts/client_code.py
+++ Code/Scripts/client_code.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     with suppress_stdout():
         x_opt, y_err = optimizer.ego('dense',func, 60000, 6, 15)
     
-    print(f'Opt params:\nepohs = {int(x_opt[0])}\nbatch = {int(x_opt[1])}\nencoded dim = {int(x_opt[2])}\nsample split = {x_opt[3]*100:.2f} % : {(1.0 - x_opt[3])*100:.2f} %')
+    print(f'Opt params:\nepochs = {int(x_opt[0])}\nbatch = {int(x_opt[1])}\nencoded dim = {int(x_opt[2])}\nsample split = {x_opt[3]*100:.2f} % : {(1.0 - x_opt[3])*100:.2f} %')
     print(f'Opt Y error: {y_err}')
     
