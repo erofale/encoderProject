4cf764ab2a9a25abf88224197a9ab54290951440
Artyom <ar.buyanow@yandex.ru>
2022-01-21 17:02:36 +0300

    training fix

--- Code/Scripts/training_models.py
+++ Code/Scripts/training_models.py
@@ -19,10 +19,12 @@ def suppress_stdout():
 def createParser ():
     parser = argparse.ArgumentParser(prog = 'training_models',
             description = 'Запуск подбора параметров выбранного автоэнкодера для выбранной функции')
-    parser.add_argument ('-f', '--func', choices = TestFunctions.get_func_names() + ['all'],
+    parser.add_argument ('-f', '--func', nargs='?', choices = TestFunctions.get_func_names() + ['all'],
                          default = 'all', type = str, help = 'Название функции')
-    parser.add_argument ('-a', '--aec', choices = AutoencoderClass.get_aec_types() + ['all'],
+    parser.add_argument ('-a', '--aec', nargs='?', choices = AutoencoderClass.get_aec_types() + ['all'],
                          default = 'all', type = str, help = 'Тип автоэнкодера')
+    parser.add_argument ('-i', '--iter', nargs='?',
+                         default = 25, type = int, help = 'Количество эпох подбора гиперпараметров автоэнкодера')
     return parser
 
 
@@ -31,6 +33,7 @@ if __name__ == "__main__":
     namespace = parser.parse_args(sys.argv[1:])
     f_name = namespace.func
     aec_type = namespace.aec
+    it = namespace.iter
     
     func_names = TestFunctions.get_func_names()
     aec_types = AutoencoderClass.get_aec_types()
@@ -44,14 +47,12 @@ if __name__ == "__main__":
             if(aec_type == 'all'):
                 for aec in aec_types:
                     with suppress_stdout():
-                        #x_opt, y_err = optimizer.ego(aec ,func, 60000, dim + 1, 100)
-                        x_opt, y_err = optimizer.ego(aec ,func, 10000, 2, 2)
+                        x_opt, y_err = optimizer.ego(aec ,func, 60000, dim + 1, it)
                     print(f'{f} {aec} training\nOpt params:\nepochs = {int(x_opt[0])}\nbatch = {int(x_opt[1])}\nencoded dim = {int(x_opt[2])}\nsample split = {x_opt[3]*100:.2f} % : {(1.0 - x_opt[3])*100:.2f} %')
                     print(f'Opt mean Y error: {y_err}\n')
             else:
                 with suppress_stdout():
-                    #x_opt, y_err = optimizer.ego(aec_type ,func, 60000, dim + 1, 100)
-                    x_opt, y_err = optimizer.ego(aec_type ,func, 10000, 2, 2)
+                    x_opt, y_err = optimizer.ego(aec_type ,func, 60000, dim + 1, it)
                 print(f'{f} {aec_type} training\nOpt params:\nepochs = {int(x_opt[0])}\nbatch = {int(x_opt[1])}\nencoded dim = {int(x_opt[2])}\nsample split = {x_opt[3]*100:.2f} % : {(1.0 - x_opt[3])*100:.2f} %')
                 print(f'Opt mean Y error: {y_err}\n')
     
@@ -62,13 +63,11 @@ if __name__ == "__main__":
         if(aec_type == 'all'):
             for aec in aec_types:
                 with suppress_stdout():
-                    #x_opt, y_err = optimizer.ego(aec ,func, 60000,dim + 1, 100)
-                    x_opt, y_err = optimizer.ego(aec ,func, 10000, 2, 2)
+                    x_opt, y_err = optimizer.ego(aec ,func, 60000,dim + 1, it)
                 print(f'{f_name} {aec} training\nOpt params:\nepochs = {int(x_opt[0])}\nbatch = {int(x_opt[1])}\nencoded dim = {int(x_opt[2])}\nsample split = {x_opt[3]*100:.2f} % : {(1.0 - x_opt[3])*100:.2f} %')
                 print(f'Opt mean Y error: {y_err}\n')
         else:
             with suppress_stdout():
-                #x_opt, y_err = optimizer.ego(aec_type ,func, 60000,dim + 1, 100)
-                x_opt, y_err = optimizer.ego(aec_type ,func, 10000, 2, 2)
+                x_opt, y_err = optimizer.ego(aec_type ,func, 60000,dim + 1, it)
             print(f'{f_name} {aec_type} training\nOpt params:\nepochs = {int(x_opt[0])}\nbatch = {int(x_opt[1])}\nencoded dim = {int(x_opt[2])}\nsample split = {x_opt[3]*100:.2f} % : {(1.0 - x_opt[3])*100:.2f} %')
             print(f'Opt mean Y error: {y_err}\n')
